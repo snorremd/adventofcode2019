706b30e47f1ef5fd74c5209acf242eaa06585802
Snorre Magnus Davøen <snorre.davoen@nsd.no>
2019-12-05 11:33:57 +0100

    Day 2 solution using recursive int-machine function
    The day 2 solution uses a recursive function to implement the int machine. The
    function takes the initial instruction ptr and memory as input and recursively
    executes instructions until the 99 terminate operation code is encountered. It
    then returns the memory where position 0 contains the answer.
    
    The second task is implemented by first constructing tuples for all combinations
    of the two ranges 0 - 99. This is done using the for function. The tuples are
    then mapped over in parallel using pmap, where each tuple is used as arguments
    to the int-machine function. We then drop results until the result equals the
    number we are looking for. Finally we return the result of (100 * noun) + verb.
    
    For this task the as-> threading macro is use to make the threading easier
    considering the forms should be threaded in different positions for many of the
    functions being called.
    
    Signed-off-by: Snorre Magnus Davøen <snorre.davoen@nsd.no>

---
+++ src/day2.clj
@@ -0,0 +1,46 @@
+(ns day2
+  (:require [clojure.string :as string]))
+
+
+(defn int-machine
+  [instr-ptr memory]
+  (let [[opcode ptr1 ptr2 ptr3] (subvec memory instr-ptr)
+        op                      (case opcode 1 + 2 * identity)]
+    (if (= opcode 99)
+      memory
+      (recur (+ instr-ptr 4)
+             (assoc memory
+                    ptr3
+                    (op (memory ptr1)
+                        (memory ptr2)))))))
+
+
+(comment
+
+  ;; Task 1
+  (as-> (slurp "./input/day2.txt") x
+    (string/split x #",")
+    (mapv read-string x)
+    (assoc x 1 12)
+    (assoc x 2 2)
+    (int-machine 0 x)
+    (first x))
+
+  ;; Task 2
+  (as-> (slurp "./input/day2.txt") res
+    (string/split res #",")
+    (mapv read-string res)
+    (vec res)
+    (for [x (range 100) y (range 100)] [res x y])
+    (pmap (fn [[memory x y]]
+            [x y
+             (-> memory
+                 (assoc 1 x)
+                 (assoc 2 y)
+                 (#(int-machine 0 %))
+                 first)])
+          res)
+    (drop-while #(not= (get % 2) 19690720) res)
+    (first res)
+    (+ (* 100 (first res))
+       (second res))))
